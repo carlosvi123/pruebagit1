4cde882485b11bf0cd5e6425218497af5742322a
Enrique Pérez Olivares <enriquejpo@ext.zara.com>
2020-06-02 08:44:48 +0200

    Added circuit breaker.

--- src/main/java/es/soprasteria/formacion/service/PersonServiceImpl.java
+++ src/main/java/es/soprasteria/formacion/service/PersonServiceImpl.java
@@ -1,5 +1,7 @@
 package es.soprasteria.formacion.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import es.soprasteria.formacion.dto.PersonDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +20,22 @@ public class PersonServiceImpl implements PersonService {
     this.restTemplate = new RestTemplate();
   }
 
+  @HystrixCommand(fallbackMethod = "fallbackNif", commandProperties = {
+      @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4"),
+      @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+      @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+  })
   @Override
   public PersonDto getByNif(String nif) {
     ResponseEntity<PersonDto> personResponse = this.restTemplate.getForEntity(baseResource + nif, PersonDto.class);
     return personResponse.getBody();
+  }
+
+  private PersonDto fallbackNif(String nif) {
+    PersonDto personDto = new PersonDto();
+    personDto.setFullName("Not available");
+    personDto.setAge(-1);
+    personDto.setNif(nif);
+    return personDto;
   }
 }
--- src/main/java/es/soprasteria/formacion/service/SkillServiceImpl.java
+++ src/main/java/es/soprasteria/formacion/service/SkillServiceImpl.java
@@ -1,6 +1,10 @@
 package es.soprasteria.formacion.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import es.soprasteria.formacion.dto.SkillDto;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,9 +21,21 @@ public class SkillServiceImpl implements SkillService {
     this.restTemplate = new RestTemplate();
   }
 
+  @HystrixCommand(fallbackMethod = "fallbackSkill", commandProperties = {
+      @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4"),
+      @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+      @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+  })
   @Override
   public SkillDto getSkill(String nif) {
     ResponseEntity<SkillDto> response = restTemplate.getForEntity(baseResource + nif, SkillDto.class);
     return response.getBody();
+  }
+
+  private SkillDto fallbackSkill(String nif) {
+    SkillDto skillDto = new SkillDto();
+    skillDto.setNif(nif);
+    skillDto.setSkills(Arrays.asList("No skills available."));
+    return skillDto;
   }
 }
